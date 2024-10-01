4d531128f3ff22b4addecf87f06b9af15295295f
Tali Auster <me@tali.network>
2024-10-01 14:13:47 -0700

    init commit

---
+++ flake.nix
@@ -0,0 +1,45 @@
+{
+  inputs.nixpkgs.url = "github:nixos/nixpkgs";
+
+  outputs = { self, nixpkgs }: {
+    packages.x86_64-linux =
+      let
+        pkgs = nixpkgs.legacyPackages.x86_64-linux;
+      in with pkgs; {
+        alire = stdenv.mkDerivation (finalAttrs: {
+          pname = "alire";
+          version = "2.1.0-dev";
+
+          src = fetchFromGitHub {
+            owner = "atalii";
+            repo = "alire";
+            rev = "d18367d58f24cd11e316f50540cef30b5823bd6a";
+            hash = "sha256-46PwpF1/ZWJmGU/GPbTMuuFRLxazlfFWuFh4VioDNJs=";
+
+            fetchSubmodules = true;
+          };
+
+          nativeBuildInputs = [ gprbuild gnat ];
+
+          buildPhase = ''
+            runHook preBuild
+            patchShebangs dev/build.sh scripts/version-patcher.sh
+
+            export ALIRE_BUILD_JOBS="$NIX_BUILD_CORES"
+            ./dev/build.sh
+
+            runHook postBuild
+          '';
+
+          installPhase = ''
+            runHook preInstall
+
+            mkdir -p $out
+            cp -r ./bin $out
+
+            runHook postInstall
+          '';
+        });
+      };
+  };
+}
