a29a2a3bff7ae3f9ae7021aa3b9cdda6747d8eb8
Tali Auster <me@tali.network>
2024-10-07 13:40:51 -0700

    helloworld: make build work
    This Frakensteins the index into the build dir. It kind of barely
    works and is exceedingly brittle. Don't worry about it.

--- flake.nix
+++ flake.nix
@@ -1,13 +1,18 @@
 {
   inputs.nixpkgs.url = "github:nixos/nixpkgs";
 
-  outputs = { self, nixpkgs }: {
+  inputs.alire-community-index = {
+    url = "github:alire-project/alire-index";
+    flake = false;
+  };
+
+  outputs = { self, nixpkgs, alire-community-index }: {
     packages.x86_64-linux =
       let
         pkgs = nixpkgs.legacyPackages.x86_64-linux;
       in with pkgs; {
         helloworld = import ./helloworld {
-          inherit pkgs;
+          inherit pkgs alire-community-index;
           alire = self.packages.x86_64-linux.alire;
         };
 
--- helloworld/default.nix
+++ helloworld/default.nix
@@ -1,17 +1,25 @@
-{ pkgs, alire }:
+{ pkgs, alire, alire-community-index }:
 with pkgs; stdenv.mkDerivation {
   pname = "helloworld";
   version = "0.1.0";
   src = ./.;
 
-  nativeBuildInputs = [ pkgs.gprbuild pkgs.gnat alire ];
+  nativeBuildInputs = [ pkgs.gprbuild pkgs.gnat alire git ];
 
   # TODO: is configurePhase correct here?
   configurePhase = ''
+    mkdir -p /tmp/.config/alire/indexes/community
+    cp -r ${alire-community-index} /tmp/.config/alire/indexes/community/repo
+    chmod +w -R /tmp/.config
+
+    # ew gross hack
+    echo 'url = "git+https://github.com/alire-project/alire-index#stable-1.3.0"' >> /tmp/.config/alire/indexes/community/index.toml
+    echo 'name = "community"' >> /tmp/.config/alire/indexes/community/index.toml
+    echo 'priority = 1' >> /tmp/.config/alire/indexes/community/index.toml
+
     alr settings --set index.auto_update 0
-    alr settings --set index.auto_community false
     alr settings --set toolcahin.assisstant false
-    alr --force toolchain --select gnat_external
+    alr --force -v toolchain --select gnat_external
   '';
 
   buildPhase = ''
@@ -19,6 +27,7 @@ with pkgs; stdenv.mkDerivation {
   '';
 
   installPhase = ''
+    mkdir -p $out
     cp -r bin $out/bin
   '';
 }
