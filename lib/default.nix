ba18c308cb1ea63aa6e3f4cd4cf154d15ea39afa
Tali Auster <me@tali.network>
2024-11-02 12:22:37 -0700

    feat: make .#withdeps build; break everything else
    Mostly scratch work and ugly code to make this damn thing work. I'll
    clean up later™.

--- lib/default.nix
+++ lib/default.nix
@@ -1,4 +1,35 @@
-{
+rec {
+  fetchDeps = { pkgs }: { src, pname, version, index, alire }:
+    with pkgs; stdenv.mkDerivation {
+      inherit src version;
+      pname = "${pname}-deps";
+
+      nativeBuildInputs = [ cacert gprbuild gnat git alire ];
+
+      configurePhase = ''
+        mkdir -p /tmp/.config/
+
+        cp -r ${index} /tmp/.config/alire/
+        chmod +w -R /tmp/.config # why isn't it created writable??
+        alr settings --set index.auto_update 0
+        alr settings --set toolcahin.assisstant false
+        alr settings --set warning.old_index false
+        alr -vv toolchain --select gnat_external
+      '';
+
+      buildPhase = ''
+        alr build --stop-after=pre-build
+      '';
+
+      installPhase = ''
+        cp -r /tmp/.local/share/alire $out
+      '';
+
+      outputHashMode = "recursive";
+      outputHashAlgo = null;
+      outputHash = "sha256-gpRL7/ma+cHVprf+Yl7+pIt5n0VtrPkvt3y2OFUaKD0=";
+  };
+
   buildAlireCrate = { pkgs }: { src, pname, version, index, alire }:
     with pkgs; stdenv.mkDerivation {
       inherit src pname version;
@@ -17,6 +48,11 @@
       '';
 
       buildPhase = ''
+        mkdir -p /tmp/.local/share
+        cp -r ${(fetchDeps { inherit pkgs; }) { inherit src pname version index alire; }} \
+          /tmp/.local/share/alire
+        chmod +w -R /tmp/.local/share/alire
+
         alr -n -v build
       '';
 
