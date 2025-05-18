bc651b3d4731facb4cfdd831eb32518c032e5139
Tali Auster <me@tali.network>
2025-05-18 09:59:54 -0700

    chore: don't use flake-utils

--- flake.nix
+++ flake.nix
@@ -1,7 +1,6 @@
 {
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs";
-    flake-utils.url = "github:numtide/flake-utils";
 
     alire-community-index = {
       url = "github:alire-project/alire-index/";
@@ -13,42 +12,39 @@
     {
       self,
       nixpkgs,
-      flake-utils,
       alire-community-index,
     }:
-    (
-      flake-utils.lib.eachDefaultSystem (system: {
-        packages =
-          let
-            pkgs = nixpkgs.legacyPackages.${system};
-          in
-          {
-            community-index = pkgs.callPackage self.lib.indexDerivation {
-              idxSrc = alire-community-index;
-              pname = "community";
-              version = "stable-1.3.0";
-            };
+    let
+      systems = nixpkgs.lib.systems.flakeExposed;
+      forAllSystems = nixpkgs.lib.genAttrs systems;
+      define = f: forAllSystems (system: f nixpkgs.legacyPackages.${system});
+    in
+    {
+      packages = define (pkgs: {
+        community-index = pkgs.callPackage self.lib.indexDerivation {
+          idxSrc = alire-community-index;
+          pname = "community";
+          version = "stable-1.3.0";
+        };
 
-            helloworld = import ./helloworld {
-              inherit pkgs;
-              alirenix = self;
-            };
+        helloworld = import ./helloworld {
+          inherit pkgs;
+          alirenix = self;
+        };
 
-            withdeps = import ./withdeps {
-              inherit pkgs;
-              alirenix = self;
-            };
+        withdeps = import ./withdeps {
+          inherit pkgs;
+          alirenix = self;
+        };
 
-            withpinned = import ./withpinned {
-              inherit pkgs;
-              alirenix = self;
-            };
+        withpinned = import ./withpinned {
+          inherit pkgs;
+          alirenix = self;
+        };
 
-            alire = pkgs.alire;
-          };
-      })
-      // {
-        lib = import ./lib;
-      }
-    );
+        alire = pkgs.alire;
+      });
+
+      lib = import ./lib;
+    };
 }
